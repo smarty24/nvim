ad0dc251e8a7514ec4387a6e896c11a3271f5064
smartiano24 <smartiano24@yahoo.com>
2024-07-13 21:32:33 +0200

    move leader key to from keymaps to lazy.lua file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,3 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
 vim.opt.backspace = "2"
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
+
 vim.opt.rtp:prepend(lazypath)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 require("lazy").setup({
   spec = {
