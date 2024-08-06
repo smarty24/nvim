4d425bf7832f4707a106b62da24f7c395646eedf
smartiano24 <smartiano24@yahoo.com>
2024-08-06 08:17:49 +0200

    rename custom_plugin file to examples and extracted needed config

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,30 @@
+return {
+
+  -- add any tools you want to have installed below
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "alex",
+        "alflake8",
+        "algrammarly-languageserver grammarly",
+        "alhelm-ls helm_ls",
+        "aljson-lsp jsonls",
+        "allua-language-server lua_ls",
+        "almarkdownlint",
+        "alprettier",
+        "alpylint",
+        "alpyright",
+        "alpython-lsp-server pylsp",
+        "alshellcheck",
+        "alshfmt",
+        "alstylua",
+        "alvtsls",
+      },
+    },
+  },
+}
