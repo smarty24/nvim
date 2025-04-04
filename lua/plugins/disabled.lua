73ec4033d7e6bae8cc2e5b4f39ad7ff7a3ed337b
smartiano24 <smartiano24@yahoo.com>
2025-04-05 00:08:37 +0200

    disabled neo-tree and activated nvim-tree

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   -- disable neo-tree
-  { "nvim-neo-tree/neo-tree.nvim", enabled = true },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 
   -- default lazyvim plugin
-  { "nvim-tree/nvim-tree.lua", enabled = false },
+  { "nvim-tree/nvim-tree.lua", enabled = true },
 }
